c1d4dd07ef17925f3d76388eecfc24b9bfcb308c
benicio <ujhmt@student.kit.edu>
2023-02-08 20:08:23 +0100

    Initial Commit

---
+++ block time/block_timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  block_timeApp.swift
+//  block time
+//
+//  Created by Benicio Nell on 08.02.23.
+//
+
+import SwiftUI
+
+@main
+struct block_timeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
