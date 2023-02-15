2512f4c723943741ff06b7e7c5d366e6e0091a32
benicio <benicio@web.de>
2023-02-15 12:05:49 +0100

    Added halving difficulty clips in progress view

---
+++ block time/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  block time
+//
+//  Created by Benicio Nell on 11.02.23.
+//
+
+import Foundation
--- block time/ProgressThingyView.swift
+++ block time/ProgressThingyView.swift
@@ -9,17 +9,23 @@ import SwiftUI
 
 struct ProgressThingyView: View {
     @EnvironmentObject var blockTip: BlockTip
+    let diffAdjust = 2016
+    let halving = 210000
     
     var body: some View {
         VStack {
             
-            ProgressView("Blocks until next difficutly adjustment: \(2016 - (blockTip.height % 2016))",
-                         value: Double(blockTip.height % 2016), total: 2016)
+            ProgressView("Blocks until next difficutly adjustment: \(diffAdjust - (blockTip.height % diffAdjust))",
+                         value: Double(blockTip.height % diffAdjust), total: Double(diffAdjust))
             .padding(.all)
             
             
-            ProgressView("Blocks until next halving: \(840000 - blockTip.height)",
-                         value: Double(blockTip.height)-630000, total: 210000)
+            ProgressView("Blocks until next halving: \(halving - (blockTip.height % halving))",
+                         value: Double(blockTip.height % halving), total: Double(halving))
+            .padding(.all)
+            
+            ProgressView("Blocks until next halving eclips: \((halving*24) - (blockTip.height % (halving*24)))",
+                         value: Double(blockTip.height % (halving*24)), total: Double(halving*24))
             .padding(.all)
         }
     }
